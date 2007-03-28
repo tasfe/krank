57993c4060bfb3a3ddf51456c80c35502dae42e0
rickh <rickh@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-03-28 08:06:38 +0000

    binaries

---
+++ external-artifacts/jsf/jsf-1.2_04-b10-p01/lib/install.bat
@@ -0,0 +1,3 @@
+call mvn install:install-file -DgroupId=javax.faces -DartifactId=jsf-impl -Dversion=1.2_04-b10-p01 -Dpackaging=jar -Dfile=jsf-impl.jar
+call mvn install:install-file -DgroupId=javax.faces -DartifactId=jsf-api  -Dversion=1.2_04-b10-p01 -Dpackaging=jar -Dfile=jsf-api.jar
+

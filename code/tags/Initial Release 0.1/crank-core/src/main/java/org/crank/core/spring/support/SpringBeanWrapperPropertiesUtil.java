07f442ce619aed8944fd0d35605cc7a7248f41e9
chrismathiaster <chrismathiaster@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-04-10 21:43:27 +0000

    [maven-scm] copy for tag Initial Release 0.1

---
+++ code/tags/Initial Release 0.1/crank-core/src/main/java/org/crank/core/spring/support/SpringBeanWrapperPropertiesUtil.java
@@ -0,0 +1,36 @@
+package org.crank.core.spring.support;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.crank.core.PropertiesUtil;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class SpringBeanWrapperPropertiesUtil implements PropertiesUtil {
+
+	public void copyProperties(Object object, Map<String, Object> properties) {
+		BeanWrapper wrapper = new BeanWrapperImpl(object);
+		Set<Entry<String, Object>> props = properties.entrySet();
+		for (Entry<String, Object> entry : props) {
+				if (wrapper.isWritableProperty(entry.getKey())) {
+					wrapper.setPropertyValue(entry.getKey(), entry.getValue());
+				}
+		}
+	}
+	
+	public Map<String, Object> getObjectPropertiesAsMap(Object object) {
+		BeanWrapper wrapper = new BeanWrapperImpl(object);
+		Map<String, Object> properties = new HashMap<String, Object>();
+		PropertyDescriptor[] propertyDescriptors = wrapper.getPropertyDescriptors();
+		for (PropertyDescriptor pd : propertyDescriptors) {
+			String name = pd.getName();
+			properties.put(name, wrapper.getPropertyValue(name));
+		}
+		return properties;
+	}
+
+}
---
+++ code/tags/Initial Release 0.1/crank-validation/src/test/java/org/crank/validation/validators/CompositeValidatorTest.java
@@ -0,0 +1,61 @@
+package org.crank.validation.validators;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.crank.validation.FieldValidator;
+import org.crank.validation.ValidatorMessage;
+import org.crank.validation.ValidatorMessages;
+
+/**
+ * Test composite validator.
+ *
+ * <p>
+ * <small>
+ * $File: //depot/projects/site/main/src/test/java/net/lmb/model/validator/impl/CompositeValidatorTest.java $ <br/>
+ * $Change: 14144 $ submitted by $Author: rhightower $ at $DateTime: 2005/06/30 18:01:41 $
+ * </small>
+ * </p>
+ * @author $Author: rhightower $
+ * @version $Revision: #1 $
+ */
+public class CompositeValidatorTest {
+
+    private CompositeValidator validator = new CompositeValidator();
+    private List<FieldValidator> validatorList = new ArrayList<FieldValidator>();
+
+    private FieldValidator pass1 = new FieldValidator() {
+
+        public ValidatorMessage validate(Object object, String fieldLabel) {
+            return new ValidatorMessage();
+        }
+
+    };
+
+
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        validator = new CompositeValidator();
+        validatorList = new ArrayList<FieldValidator>();
+        validatorList.add(pass1);
+        validatorList.add(pass1);
+        validator.setList(validatorList);
+    }
+
+    @Test()
+	public void testCompositePass() {
+
+        ValidatorMessages messages = (ValidatorMessages) validator.validate("crap", "fieldLabel");
+        assertNotNull(messages);
+
+        for (ValidatorMessage message : messages) {
+            assertNotNull(message);
+            assertFalse(message.hasError());
+        }
+
+    }
+
+}

1bd5b9f12e53e8d5ea4d2041eae00a6c7c842160
taboraz <taboraz@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-08-17 01:53:34 +0000

    Added enum label generator for rendering enum constants in a pretty manner.

--- code/trunk/validation/crank-core/src/main/java/org/crank/core/StringUtils.java
+++ code/trunk/validation/crank-core/src/main/java/org/crank/core/StringUtils.java
@@ -1,5 +1,6 @@
 package org.crank.core;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +16,20 @@ public class StringUtils {
 
     public static List<String> split(String str) {
         return Arrays.asList( str.split( "," ) );
+    }    
+    
+    public static List<String> splitProperty(String str) {
+    	
+    	String prop1 = str.substring(0,str.indexOf("."));
+    	String prop2 = str.substring(str.indexOf(".") + 1, str.length());
+    	
+    	List<String> results = new ArrayList();
+    	
+    	results.add(prop1);
+    	results.add(prop2);
+    	
+    	return results;
+        
     }    
     
     public static String unCapitalize (String string) {

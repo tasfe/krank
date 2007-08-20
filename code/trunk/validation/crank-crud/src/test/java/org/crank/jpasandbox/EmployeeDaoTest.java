8cb4d1ef818aaf926390a2597b115fb4b0cfc2a6
chrismathiaster <chrismathiaster@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-08-20 21:11:41 +0000

    commenting out sandbox test code

--- code/trunk/validation/crank-crud/src/test/java/org/crank/jpasandbox/EmployeeDaoTest.java
+++ code/trunk/validation/crank-crud/src/test/java/org/crank/jpasandbox/EmployeeDaoTest.java
@@ -10,28 +10,28 @@ import org.testng.annotations.Test;
 public class EmployeeDaoTest {
     
     private ApplicationContext applicationContext;
-    @BeforeClass
-    public void setup() {
-        applicationContext = new ClassPathXmlApplicationContext( "org/crank/jpasandbox/applicationContext.xml" );
-    }
-    
-    @Test
-    public void testCreateObject() {
-        EmployeeDao employeeDao = (EmployeeDao) applicationContext.getBean( "employeeDao" );
-        Employee employee = new Employee();
-        employee.setAge( 5 );
-        employee.setDescription( "Ugly" );
-        employee.setFirstName( "Joe" );
-        employee.setLastName( "Barone" );
-        employee.setNumberOfPromotions( -1 );
-        employee.setStatus( EmployeeStatus.HOURLY );
-        employeeDao.create( employee );
-        
-        System.out.println("About to check for id");
-       // assert employee.getId() != null && employee.getId() > 0;
-        System.out.println("ID CHecked");
-        
-        //employeeDao.delete( employee );
-        
-    }
+//    @BeforeClass
+//    public void setup() {
+//        applicationContext = new ClassPathXmlApplicationContext( "org/crank/jpasandbox/applicationContext.xml" );
+//    }
+//    
+//    @Test
+//    public void testCreateObject() {
+//        EmployeeDao employeeDao = (EmployeeDao) applicationContext.getBean( "employeeDao" );
+//        Employee employee = new Employee();
+//        employee.setAge( 5 );
+//        employee.setDescription( "Ugly" );
+//        employee.setFirstName( "Joe" );
+//        employee.setLastName( "Barone" );
+//        employee.setNumberOfPromotions( -1 );
+//        employee.setStatus( EmployeeStatus.HOURLY );
+//        employeeDao.create( employee );
+//        
+//        System.out.println("About to check for id");
+//       // assert employee.getId() != null && employee.getId() > 0;
+//        System.out.println("ID CHecked");
+//        
+//        //employeeDao.delete( employee );
+//        
+//    }
 }

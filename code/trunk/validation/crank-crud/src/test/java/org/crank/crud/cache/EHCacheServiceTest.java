bfcadb7f772a77cb300e6753d157d16584dd8e16
chrismathiaster <chrismathiaster@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-08-23 03:02:33 +0000

    Comitting caching and preloading services, lazy-init fix in springtestngbase, and jndi capabilities.

---
+++ code/trunk/validation/crank-crud/src/test/java/org/crank/crud/cache/EHCacheServiceTest.java
@@ -0,0 +1,79 @@
+package org.crank.crud.cache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.crank.crud.test.DbUnitTestBase;
+import org.crank.crud.test.dao.EmployeeDAO;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class EHCacheServiceTest extends DbUnitTestBase {
+   
+    @Override
+    protected boolean isSetupJndiContext() {
+        return true;
+    }
+    
+    @Override
+    protected boolean isIsolateConfigs() {
+        return true;
+    }
+
+    @Override
+    public List<String> getConfigLocations() {
+        List<String> configs = new ArrayList<String>();
+        configs.add( "org/crank/crud/cache/spring/applicationContext.xml" );
+        return configs;
+    }
+
+    @Override
+    public String getDataSetXml() {
+        return "data/Employee.xml";
+    }
+    
+    @AfterTest
+    public void shutdown() {
+        ehCacheService.stop();
+    }
+
+    private EmployeeDAO preloadingEmployeeDao;
+    private EHCacheService ehCacheService;
+
+    public void setEhCacheService( EHCacheService ehCacheService ) {
+        this.ehCacheService = ehCacheService;
+    }
+
+    public void setPreloadingEmployeeDao( EmployeeDAO preloadingEmployeeDao ) {
+        this.preloadingEmployeeDao = preloadingEmployeeDao;
+    }
+    
+    @Test
+    public void testCreateCache() {
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.setCacheName( "FooBar" );
+        ehCacheService.createCache( cacheConfiguration );
+        assert ehCacheService.hasCache( "FooBar" );
+    }
+    
+    @Test
+    public void testRemoveCache() {
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.setCacheName( "FooBar" );
+        ehCacheService.createCache( cacheConfiguration );
+        assert ehCacheService.hasCache( "FooBar" );
+        ehCacheService.expireCache( "FooBar" );
+        assert !ehCacheService.hasCache( "FooBar" );
+    }
+    
+    @Test
+    public void testHasCache() {
+        //from preloading employee dao should already have cache of org.crank.crud.test.model.Employee
+        assert ehCacheService.hasCache( "org.crank.crud.test.model.Employee" );
+    }
+    
+    @Test
+    public void testListCaches() {
+        assert ehCacheService.listCaches().size() > 0;
+    }
+}

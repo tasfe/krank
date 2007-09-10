6a630d28aed8afd91e936f379d2344e777363f25
bill.dudney <bill.dudney@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-09-10 21:39:34 +0000

    more repo redo work

--- external-artifacts/jsf/jsf-1.2_04-b10-p01/lib/install.bat
+++
@@ -1,3 +0,0 @@
-call mvn install:install-file -DgroupId=javax.faces -DartifactId=jsf-impl -Dversion=1.2_04-b10-p01 -Dpackaging=jar -Dfile=jsf-impl.jar
-call mvn install:install-file -DgroupId=javax.faces -DartifactId=jsf-api  -Dversion=1.2_04-b10-p01 -Dpackaging=jar -Dfile=jsf-api.jar
-

d19dfabeaea463dac715b6fc5edb9c1c1e83ed11
bill.dudney <bill.dudney@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-10-04 13:11:09 +0000

    more clean up of jpa stuff, made DbUnitTestBase hibernate independent, TestThis had nothing in it so I deleted it

--- trunk/crank-crud/src/test/java/org/crank/crud/test/DbUnitTestBase.java
+++ trunk/crank-crud/src/test/java/org/crank/crud/test/DbUnitTestBase.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
 import org.apache.log4j.Logger;
 import org.dbunit.DefaultDatabaseTester;
 import org.dbunit.IDatabaseTester;
@@ -17,7 +20,8 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.XmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
-import org.hibernate.Session;
+import java.sql.Connection;
+import java.sql.SQLException;
 import org.testng.annotations.BeforeClass;
 
 /**
@@ -49,28 +53,66 @@ public abstract class DbUnitTestBase extends SpringTestNGBase {
         return new FilteredDataSet( filter, data );
     }
 
-    @BeforeClass
-    protected void initPersistenceStuff() throws Exception {
-        EntityManager em = null;
-        try {
-            em = entityManagerFactory.createEntityManager();
-            Session hibernateSession = (Session) em.getDelegate();
-            Connection conn = hibernateSession.connection();
-            IDatabaseConnection dbunitConn = new DatabaseConnection( conn );
-            IDataSet dataSet = loadDataSet( dbunitConn );
-            databaseTester = new DefaultDatabaseTester( dbunitConn );
-            databaseTester.setDataSet( dataSet );
-            databaseTester.setSetUpOperation( DatabaseOperation.CLEAN_INSERT );
-            databaseTester.onSetup();
-        } catch (Exception ex) {
-            log.debug( "Exception in initializing database", ex );
-            throw ex;
-        } finally {
-            if (em != null) {
-                em.close();
-            }
-        }
-    }
+	private Connection getWithOpenJPASession(Object delegate)
+			throws SecurityException, NoSuchMethodException,
+			IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException, SQLException {
+		Method method = delegate.getClass().getMethod("getConnection",
+				(Class[]) null);
+		Object connDelegate = method.invoke(delegate, (Object[]) null);
+		Method method2 = connDelegate.getClass().getMethod("getDelegate", (Class[])null);
+		Connection jdbcConn = (Connection)method2.invoke(connDelegate, (Object[])null);
+		jdbcConn.setAutoCommit(true);
+		return jdbcConn;
+	}
+
+	private Connection getWithHibernateSession(Object delegate)
+			throws SecurityException, NoSuchMethodException,
+			IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException {
+		Connection conn = null;
+		Method method = delegate.getClass().getMethod("connection",
+				(Class[]) null);
+		conn = (Connection) method.invoke(delegate, (Object[]) null);
+		return conn;
+	}
+
+	private Connection getConnection(EntityManager em)
+			throws SecurityException, IllegalArgumentException,
+			NoSuchMethodException, IllegalAccessException,
+			InvocationTargetException, SQLException {
+		Connection conn = null;
+		Object delegate = (Object) em.getDelegate();
+		if (delegate.getClass().getName().equals("org.hibernate.impl.SessionImpl")) {
+			conn = getWithHibernateSession(delegate);
+		} else if(delegate.getClass().getName().equals("org.apache.openjpa.persistence.EntityManagerImpl")) {
+			conn = getWithOpenJPASession(delegate);
+		}
+		return conn;
+	}
+
+	@BeforeClass
+	protected void initPersistenceStuff() throws Exception {
+		EntityManager em = null;
+		try {
+			em = entityManagerFactory.createEntityManager();
+			Connection conn = getConnection(em);
+			assert null != conn;
+			IDatabaseConnection dbunitConn = new DatabaseConnection(conn);
+			IDataSet dataSet = loadDataSet(dbunitConn);
+			databaseTester = new DefaultDatabaseTester(dbunitConn);
+			databaseTester.setDataSet(dataSet);
+			databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+			databaseTester.onSetup();
+		} catch (Exception ex) {
+			log.debug("Exception in initializing database", ex);
+			throw ex;
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
 
     public EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;

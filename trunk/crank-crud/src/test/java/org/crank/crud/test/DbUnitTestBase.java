fda4d1b5d0599eda958448cef7d20a479711cf8f
bill.dudney <bill.dudney@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-10-09 23:50:37 +0000

    mostly formatting changes

--- trunk/crank-crud/src/test/java/org/crank/crud/test/DbUnitTestBase.java
+++ trunk/crank-crud/src/test/java/org/crank/crud/test/DbUnitTestBase.java
@@ -1,17 +1,18 @@
 package org.crank.crud.test;
 
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
-
 import org.apache.log4j.Logger;
 import org.dbunit.DefaultDatabaseTester;
 import org.dbunit.IDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.DatabaseSequenceFilter;
 import org.dbunit.database.IDatabaseConnection;
@@ -20,105 +21,118 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.XmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
-import java.sql.Connection;
-import java.sql.SQLException;
 import org.testng.annotations.BeforeClass;
 
 /**
-*
-*  @version $Revision:$
-*  @author Rick Hightower
-*/
+ * 
+ * @version $Revision:$
+ * @author Rick Hightower
+ */
 public abstract class DbUnitTestBase extends SpringTestNGBase {
 
     protected EntityManagerFactory entityManagerFactory;
     private IDatabaseTester databaseTester;
-    protected final Logger log = Logger.getLogger( this.getClass() );
+    protected final Logger log = Logger.getLogger(this.getClass());
 
-    private IDataSet loadDataSet( IDatabaseConnection connection ) throws Exception {
+    private IDataSet loadDataSet(IDatabaseConnection connection)
+            throws Exception {
         // load the data set
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        InputStream stream = cl.getResource( getDataSetXml() ).openStream();
+        InputStream stream = cl.getResource(getDataSetXml()).openStream();
 
         IDataSet data;
 
         if (getUseFlatXmlDataSet()) {
-            data = new FlatXmlDataSet( stream );
+            data = new FlatXmlDataSet(stream);
         } else {
-            data = new XmlDataSet( stream );
+            data = new XmlDataSet(stream);
         }
 
         // order the data set
-        DatabaseSequenceFilter filter = new DatabaseSequenceFilter( connection, data.getTableNames() );
-        return new FilteredDataSet( filter, data );
+        DatabaseSequenceFilter filter = new DatabaseSequenceFilter(connection,
+                data.getTableNames());
+        return new FilteredDataSet(filter, data);
     }
 
-	private Connection getWithOpenJPASession(Object delegate)
-			throws SecurityException, NoSuchMethodException,
-			IllegalArgumentException, IllegalAccessException,
-			InvocationTargetException, SQLException {
-		Method method = delegate.getClass().getMethod("getConnection",
-				(Class[]) null);
-		Object connDelegate = method.invoke(delegate, (Object[]) null);
-		Method method2 = connDelegate.getClass().getMethod("getDelegate", (Class[])null);
-		Connection jdbcConn = (Connection)method2.invoke(connDelegate, (Object[])null);
-		jdbcConn.setAutoCommit(true);
-		return jdbcConn;
-	}
+    private Connection getWithOpenJPASession(Object delegate)
+            throws SecurityException, NoSuchMethodException,
+            IllegalArgumentException, IllegalAccessException,
+            InvocationTargetException, SQLException {
+        Method method = delegate.getClass().getMethod("getConnection",
+                (Class[]) null);
+        Object connDelegate = method.invoke(delegate, (Object[]) null);
+        Method method2 = connDelegate.getClass().getMethod("getDelegate",
+                (Class[]) null);
+        Connection jdbcConn = (Connection) method2.invoke(connDelegate,
+                (Object[]) null);
+        jdbcConn.setAutoCommit(true);
+        return jdbcConn;
+    }
 
-	private Connection getWithHibernateSession(Object delegate)
-			throws SecurityException, NoSuchMethodException,
-			IllegalArgumentException, IllegalAccessException,
-			InvocationTargetException {
-		Connection conn = null;
-		Method method = delegate.getClass().getMethod("connection",
-				(Class[]) null);
-		conn = (Connection) method.invoke(delegate, (Object[]) null);
-		return conn;
-	}
+    private Connection getWithHibernateSession(Object delegate)
+            throws SecurityException, NoSuchMethodException,
+            IllegalArgumentException, IllegalAccessException,
+            InvocationTargetException {
+        Connection conn = null;
+        Method method = delegate.getClass().getMethod("connection",
+                (Class[]) null);
+        conn = (Connection) method.invoke(delegate, (Object[]) null);
+        return conn;
+    }
 
-	private Connection getConnection(EntityManager em)
-			throws SecurityException, IllegalArgumentException,
-			NoSuchMethodException, IllegalAccessException,
-			InvocationTargetException, SQLException {
-		Connection conn = null;
-		Object delegate = (Object) em.getDelegate();
-		if (delegate.getClass().getName().equals("org.hibernate.impl.SessionImpl")) {
-			conn = getWithHibernateSession(delegate);
-		} else if(delegate.getClass().getName().equals("org.apache.openjpa.persistence.EntityManagerImpl")) {
-			conn = getWithOpenJPASession(delegate);
-		}
-		return conn;
-	}
+    private Connection getConnection(EntityManager em)
+            throws SecurityException, IllegalArgumentException,
+            NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, SQLException {
+        Connection conn = null;
 
-	@BeforeClass
-	protected void initPersistenceStuff() throws Exception {
-		EntityManager em = null;
-		try {
-			em = entityManagerFactory.createEntityManager();
-			Connection conn = getConnection(em);
-			assert null != conn;
-			IDatabaseConnection dbunitConn = new DatabaseConnection(conn);
-			IDataSet dataSet = loadDataSet(dbunitConn);
-			databaseTester = new DefaultDatabaseTester(dbunitConn);
-			databaseTester.setDataSet(dataSet);
-			databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
-			databaseTester.onSetup();
-		} catch (Exception ex) {
-			log.debug("Exception in initializing database", ex);
-			throw ex;
-		} finally {
-			if (em != null) {
-				em.close();
-			}
-		}
-	}
+        Object delegate = (Object) em.getDelegate();
+        if (delegate.getClass().getName().equals(
+                "org.hibernate.impl.SessionImpl")) {
+            conn = getWithHibernateSession(delegate);
+        } else if (delegate.getClass().getName().equals(
+                "org.apache.openjpa.persistence.EntityManagerImpl")) {
+            conn = getWithOpenJPASession(delegate);
+        }
+        return conn;
+    }
+
+    @BeforeClass
+    protected void initPersistenceStuff() throws Exception {
+        EntityManager em = null;
+        try {
+            em = entityManagerFactory.createEntityManager();
+            Connection conn = getConnection(em);
+            assert null != conn;
+            IDatabaseConnection dbunitConn = new DatabaseConnection(conn);
+            // only if we are running against HSQL
+            String dbProdName = conn.getMetaData().getDatabaseProductName();
+            if (dbProdName.toLowerCase().contains("hsql")) {
+                DatabaseConfig config = dbunitConn.getConfig();
+                config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY,
+                        new HsqlDataTypeFactory());
+            }
+            IDataSet dataSet = loadDataSet(dbunitConn);
+            databaseTester = new DefaultDatabaseTester(dbunitConn);
+            databaseTester.setDataSet(dataSet);
+            databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+            databaseTester.onSetup();
+        } catch (Exception ex) {
+            log.debug("Exception in initializing database", ex);
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 
     public EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;
     }
 
-    public void setEntityManagerFactory( EntityManagerFactory vmcBizEntityManagerFactory ) {
+    public void setEntityManagerFactory(
+            EntityManagerFactory vmcBizEntityManagerFactory) {
         this.entityManagerFactory = vmcBizEntityManagerFactory;
     }
 

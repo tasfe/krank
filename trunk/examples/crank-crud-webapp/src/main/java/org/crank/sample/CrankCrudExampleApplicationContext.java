3327ca21542cd51b831f12518a5d38f8f53fa624
bill.dudney <bill.dudney@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-10-10 22:57:15 +0000

    formatting

--- trunk/examples/crank-crud-webapp/src/main/java/org/crank/sample/CrankCrudExampleApplicationContext.java
+++ trunk/examples/crank-crud-webapp/src/main/java/org/crank/sample/CrankCrudExampleApplicationContext.java
@@ -1,5 +1,9 @@
 package org.crank.sample;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.crank.config.spring.support.CrudJSFConfig;
 import org.crank.controller.ExcelExportControllerBean;
@@ -16,8 +20,20 @@ import org.crank.crud.dao.EmployeeDAO;
 import org.crank.crud.dao.RoleDAO;
 import org.crank.crud.dao.SpecialtyDAO;
 import org.crank.crud.join.Fetch;
-import org.crank.crud.jsf.support.*;
-import org.crank.crud.model.*;
+import org.crank.crud.jsf.support.AutocompleteController;
+import org.crank.crud.jsf.support.JsfCrudAdapter;
+import org.crank.crud.jsf.support.JsfDetailController;
+import org.crank.crud.jsf.support.JsfSelectManyController;
+import org.crank.crud.jsf.support.JsfSelectOneListingController;
+import org.crank.crud.jsf.support.SelectItemGenerator;
+import org.crank.crud.model.ContactInfo;
+import org.crank.crud.model.Department;
+import org.crank.crud.model.Employee;
+import org.crank.crud.model.EmployeeStatus;
+import org.crank.crud.model.Role;
+import org.crank.crud.model.Skill;
+import org.crank.crud.model.Specialty;
+import org.crank.crud.model.Task;
 import org.crank.crud.relationships.RelationshipManager;
 import org.crank.model.jsf.support.RichFacesTreeModelBuilder;
 import org.springframework.config.java.annotation.Bean;
@@ -27,70 +43,70 @@ import org.springframework.config.java.annotation.Lazy;
 import org.springframework.config.java.annotation.aop.ScopedProxy;
 import org.springframework.config.java.util.DefaultScopes;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
-@Configuration (defaultLazy=Lazy.TRUE)
+@Configuration(defaultLazy = Lazy.TRUE)
 public abstract class CrankCrudExampleApplicationContext extends CrudJSFConfig {
-    
-    private static List<CrudManagedObject> managedObjects;
-    
-	@Bean (scope = DefaultScopes.SINGLETON)    
-    public List<CrudManagedObject> managedObjects() {
-    	if (managedObjects==null) {
-	    	managedObjects = new ArrayList<CrudManagedObject>();
-	        managedObjects.add( new CrudManagedObject(Employee.class, EmployeeDAO.class) );
-	        managedObjects.add( new CrudManagedObject(Department.class, DepartmentDAO.class) );
-	        managedObjects.add( new CrudManagedObject(Specialty.class, SpecialtyDAO.class) );
-	        managedObjects.add( new CrudManagedObject(Role.class, RoleDAO.class) );
-	        managedObjects.add( new CrudManagedObject(Skill.class, null) );
-	        
-    	}
-    	return managedObjects;
-		
+
+	private static List<CrudManagedObject> managedObjects;
+
+	@Bean(scope = DefaultScopes.SINGLETON)
+	public List<CrudManagedObject> managedObjects() {
+		if (managedObjects == null) {
+			managedObjects = new ArrayList<CrudManagedObject>();
+			managedObjects.add(new CrudManagedObject(Employee.class,
+					EmployeeDAO.class));
+			managedObjects.add(new CrudManagedObject(Department.class,
+					DepartmentDAO.class));
+			managedObjects.add(new CrudManagedObject(Specialty.class,
+					SpecialtyDAO.class));
+			managedObjects
+					.add(new CrudManagedObject(Role.class, RoleDAO.class));
+			managedObjects.add(new CrudManagedObject(Skill.class, null));
+		}
+		return managedObjects;
+
 	}
 
-    @SuppressWarnings("unchecked")
-    @Bean(scope = DefaultScopes.SESSION, aliases = "pagers")
-    public Map<String, FilterablePageable> paginators() throws Exception {
-        Map<String, FilterablePageable> paginators =  super.paginators();
-        paginators.get("Employee").addOrderBy(OrderBy.asc("firstName"));
-        return paginators;
-    }
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SESSION, aliases = "pagers")
+	public Map<String, FilterablePageable> paginators() throws Exception {
+		Map<String, FilterablePageable> paginators = super.paginators();
+		paginators.get("Employee").addOrderBy(OrderBy.asc("firstName"));
+		return paginators;
+	}
 
-    @Bean (scope = DefaultScopes.SESSION)
-    @ScopedProxy
-    public DataTableScrollerBean dataTableScrollerBean() throws Exception {
-        DataTableScrollerBean bean = new DataTableScrollerBean();
-        bean.setEmployeeDataPaginator(paginators().get("Employee"));
-        bean.setEmployeeDAO(repos().get("Employee"));
-        return bean;
-    }
-    @SuppressWarnings("unchecked")
-    @Bean (scope = DefaultScopes.REQUEST)
-    public SelectEmployeeListingController selectEmployeeListingController() throws Exception {
-           SelectEmployeeListingController controller = new SelectEmployeeListingController(paginators().get("Employee"));
-           return controller;
-    }
+	@ScopedProxy
+	@Bean(scope = DefaultScopes.SESSION)
+	public DataTableScrollerBean dataTableScrollerBean() throws Exception {
+		DataTableScrollerBean bean = new DataTableScrollerBean();
+		bean.setEmployeeDataPaginator(paginators().get("Employee"));
+		bean.setEmployeeDAO(repos().get("Employee"));
+		return bean;
+	}
 
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.REQUEST)
+	public SelectEmployeeListingController selectEmployeeListingController()
+			throws Exception {
+		SelectEmployeeListingController controller = new SelectEmployeeListingController(
+				paginators().get("Employee"));
+		return controller;
+	}
 
-    @ExternalBean
-    public abstract JsfCrudAdapter deptCrud();
+	@ExternalBean
+	public abstract JsfCrudAdapter deptCrud();
 
-    @SuppressWarnings("unchecked")
-    @Bean (scope = DefaultScopes.SESSION, aliases = "deptCrud") 
-    public JsfCrudAdapter deptCrudController() throws Exception {
-        JsfCrudAdapter adapter = cruds().get( "Department");
-        adapter.getController().addChild( "employees", new JsfDetailController(Employee.class))
-        .addChild( "tasks", new JsfDetailController(Task.class) );
-        return adapter;
-    }
-    
-    @SuppressWarnings("unchecked")
-    @Bean(scope = DefaultScopes.SESSION, aliases = "empCrud")
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SESSION, aliases = "deptCrud")
+	public JsfCrudAdapter deptCrudController() throws Exception {
+		JsfCrudAdapter adapter = cruds().get("Department");
+		adapter.getController().addChild("employees",
+				new JsfDetailController(Employee.class)).addChild("tasks",
+				new JsfDetailController(Task.class));
+		return adapter;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SESSION, aliases = "empCrud")
 	public JsfCrudAdapter employeeCrud() throws Exception {
 		JsfCrudAdapter adapter = cruds().get("Employee");
 
@@ -120,113 +136,130 @@ public abstract class CrankCrudExampleApplicationContext extends CrudJSFConfig {
 		relationshipManager.setRemoveFromParentMethodName("removeDirectReport");
 
 		adapter.getController().addChild("directReports", directReports);
-		
-		
+
 		return adapter;
 	}
 
-    @ExternalBean
-    abstract JsfCrudAdapter<Employee, Long> empCrud();
+	@ExternalBean
+	abstract JsfCrudAdapter<Employee, Long> empCrud();
 
-    @Bean(scope = DefaultScopes.SESSION)
-	public JsfSelectManyController<Role, Long> employeeToRoleController() throws Exception {
-		JsfSelectManyController<Role, Long> controller = new JsfSelectManyController<Role, Long>(Role.class, "roles", paginators().get("Role"), empCrud().getController()); 
-    	return controller;
-    }
-    
+	@Bean(scope = DefaultScopes.SESSION)
+	public JsfSelectManyController<Role, Long> employeeToRoleController()
+			throws Exception {
+		JsfSelectManyController<Role, Long> controller = new JsfSelectManyController<Role, Long>(
+				Role.class, "roles", paginators().get("Role"), empCrud()
+						.getController());
+		return controller;
+	}
 
-    @Bean(scope = DefaultScopes.SESSION)
-	public JsfSelectOneListingController<Specialty, Long> employeeToSpecialtyController() throws Exception {
-    	JsfSelectOneListingController<Specialty, Long> controller = new JsfSelectOneListingController<Specialty, Long>(Specialty.class, "specialty", paginators().get("Specialty"), empCrud().getController()); 
-    	return controller;
-    }
+	@Bean(scope = DefaultScopes.SESSION)
+	public JsfSelectOneListingController<Specialty, Long> employeeToSpecialtyController()
+			throws Exception {
+		JsfSelectOneListingController<Specialty, Long> controller = new JsfSelectOneListingController<Specialty, Long>(
+				Specialty.class, "specialty", paginators().get("Specialty"),
+				empCrud().getController());
+		return controller;
+	}
 
-    @Bean(scope = DefaultScopes.SESSION)
-	public JsfSelectOneListingController<Skill, Long> employeeToSkillController() throws Exception {
-    	JsfSelectOneListingController<Skill, Long> controller = new JsfSelectOneListingController<Skill, Long>(Skill.class, "primarySkill", paginators().get("Skill"), empCrud().getController()); 
-    	return controller;
-    }
+	@Bean(scope = DefaultScopes.SESSION)
+	public JsfSelectOneListingController<Skill, Long> employeeToSkillController()
+			throws Exception {
+		JsfSelectOneListingController<Skill, Long> controller = new JsfSelectOneListingController<Skill, Long>(
+				Skill.class, "primarySkill", paginators().get("Skill"),
+				empCrud().getController());
+		return controller;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Bean (scope = DefaultScopes.SINGLETON)
-    public Map<String, SelectItemGenerator> selectItemGenerators() throws Exception {
-        Map<String, SelectItemGenerator> selectItemGenerators = super.selectItemGenerators();
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SINGLETON)
+	public Map<String, SelectItemGenerator> selectItemGenerators()
+			throws Exception {
+		Map<String, SelectItemGenerator> selectItemGenerators = super
+				.selectItemGenerators();
 
-        SelectItemGenerator selectItemGenerator = new SelectItemGenerator();
-        EnumDataSource<EmployeeStatus> dataSource = new EnumDataSource();
-        dataSource.setType(EmployeeStatus.class);
-        selectItemGenerator.setDataSource( dataSource );
-        selectItemGenerators.put("EmployeeStatus", selectItemGenerator);
-        
-        return selectItemGenerators;
-    }
-    
-    
-    @Bean (scope = DefaultScopes.SESSION)
-    @ScopedProxy
-    public ExcelExportControllerBean controllerBean() throws Exception {
-      ExcelExportControllerBean bean = new ExcelExportControllerBean();
-        return bean;
-    }
-    
-    @SuppressWarnings("unchecked")
-	@Bean (scope = DefaultScopes.SESSION)
-    @ScopedProxy
-    public TreeControllerBean treeControllerBean() throws Exception {
-    	/* Create TreeControllerBean. */
-    	TreeControllerBean bean = new TreeControllerBean();
-    	
-    	/* Setup a datasource for the tree. */
-    	DaoFilteringDataSource<Department, Long> dataSource = new DaoFilteringDataSource<Department, Long>();
-    	dataSource.setDao(this.repos().get("Department"));
-    	dataSource.setFetches(Fetch.leftJoinFetch("employees"));
-    	
-    	/* Create a tree builder for the tree model. */
-    	RichFacesTreeModelBuilder treeBuilder = new RichFacesTreeModelBuilder();
-    	/* Inject build instructions for tree nodes. */
-    	treeBuilder.setTreeBuildDirections("Departments->this.name->employees.firstName,lastName");
-    	treeBuilder.setNoRoot(false);
-    	
-    	/* Inject dependencies in tree controller. */
-    	bean.setDataSource(dataSource);
-    	bean.setTreeBuilder(treeBuilder);
-    	bean.setEmployeeCrud(this.employeeCrud());
-    	bean.setDeptCrud(this.deptCrud());
-    	return bean;
-    }
+		SelectItemGenerator selectItemGenerator = new SelectItemGenerator();
+		EnumDataSource<EmployeeStatus> dataSource = new EnumDataSource();
+		dataSource.setType(EmployeeStatus.class);
+		selectItemGenerator.setDataSource(dataSource);
+		selectItemGenerators.put("EmployeeStatus", selectItemGenerator);
 
-    @SuppressWarnings("unchecked")
-    @Bean (scope = DefaultScopes.SESSION) 
-    public Map<String, AutocompleteController> autocomplete () throws Exception {
-        Map<String, AutocompleteController> autocomplete = new HashMap<String, AutocompleteController>();
+		return selectItemGenerators;
+	}
 
-        // Add the data source which contains the auto-complete data
-        DaoFilteringDataSource dataSource = new DaoFilteringDataSource();
-        dataSource.setDao( repos().get( "Specialty" ));
-        
-        // Create the auto-complete controller for the particular property on the data source
-        // Arguments are:
-        //     dataSource -   the data source for the lookup values
-        //     propertyName - the property on the lookup results which will be used query against
-        //     fieldName -    the field name of the associated entity which will be assigned the  
-        //                    validated auto-complete entity upon successful lookup
-        AutocompleteController autoController = new AutocompleteController(dataSource, "name", "specialty");
-        
-        // Wire in the event handler to the associated controller for dealing with 
-        // conversion / validation from the Many to One association to the controller's entity
-        cruds().get("Employee").getController().addCrudControllerListener(autoController);
-        
-        // Add to the auto-complete map...
-        //     This will be accessed via the field.xhtml as the AutoComplete controller / validator
-        //     for the associated property
-        autocomplete.put("Specialty", autoController);
+	@Bean(scope = DefaultScopes.SESSION)
+	@ScopedProxy
+	public ExcelExportControllerBean controllerBean() throws Exception {
+		ExcelExportControllerBean bean = new ExcelExportControllerBean();
+		return bean;
+	}
 
-        return autocomplete;
-    }
-    
-    @Bean
-    public String persistenceUnitName() {
-    	return "employee-example"; 
-    }
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SESSION)
+	@ScopedProxy
+	public TreeControllerBean treeControllerBean() throws Exception {
+		/* Create TreeControllerBean. */
+		TreeControllerBean bean = new TreeControllerBean();
+
+		/* Setup a datasource for the tree. */
+		DaoFilteringDataSource<Department, Long> dataSource = new DaoFilteringDataSource<Department, Long>();
+		dataSource.setDao(this.repos().get("Department"));
+		dataSource.setFetches(Fetch.leftJoinFetch("employees"));
+
+		/* Create a tree builder for the tree model. */
+		RichFacesTreeModelBuilder treeBuilder = new RichFacesTreeModelBuilder();
+		/* Inject build instructions for tree nodes. */
+		treeBuilder
+				.setTreeBuildDirections("Departments->this.name->employees.firstName,lastName");
+		treeBuilder.setNoRoot(false);
+
+		/* Inject dependencies in tree controller. */
+		bean.setDataSource(dataSource);
+		bean.setTreeBuilder(treeBuilder);
+		bean.setEmployeeCrud(this.employeeCrud());
+		bean.setDeptCrud(this.deptCrud());
+		return bean;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Bean(scope = DefaultScopes.SESSION)
+	public Map<String, AutocompleteController> autocomplete() throws Exception {
+		Map<String, AutocompleteController> autocomplete = new HashMap<String, AutocompleteController>();
+
+		// Add the data source which contains the auto-complete data
+		DaoFilteringDataSource dataSource = new DaoFilteringDataSource();
+		dataSource.setDao(repos().get("Specialty"));
+
+		// Create the auto-complete controller for the particular property on
+		// the data source
+		// Arguments are:
+		// dataSource - the data source for the lookup values
+		// propertyName - the property on the lookup results which will be used
+		// query against
+		// fieldName - the field name of the associated entity which will be
+		// assigned the
+		// validated auto-complete entity upon successful lookup
+		AutocompleteController autoController = new AutocompleteController(
+				dataSource, "name", "specialty");
+
+		// Wire in the event handler to the associated controller for dealing
+		// with
+		// conversion / validation from the Many to One association to the
+		// controller's entity
+		cruds().get("Employee").getController().addCrudControllerListener(
+				autoController);
+
+		// Add to the auto-complete map...
+		// This will be accessed via the field.xhtml as the AutoComplete
+		// controller / validator
+		// for the associated property
+		autocomplete.put("Specialty", autoController);
+
+		return autocomplete;
+	}
+
+	@Bean
+	public String persistenceUnitName() {
+		return "employee-example";
+	}
 
 }

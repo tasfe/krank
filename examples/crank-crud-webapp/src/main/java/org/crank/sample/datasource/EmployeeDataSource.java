f3c3371d98517e7e4e3512b04fed36287bdc2e3f
RichardHightower <RichardHightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-11-06 21:55:02 +0000

    Added comments, fixed minor bug with multi-filters

--- examples/crank-crud-webapp/src/main/java/org/crank/sample/datasource/EmployeeDataSource.java
+++ examples/crank-crud-webapp/src/main/java/org/crank/sample/datasource/EmployeeDataSource.java
@@ -14,14 +14,128 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class EmployeeDataSource implements FilteringPagingDataSource {
 	
+	/** The group holds conditions used by the listing to filter results. */
 	private Group group = new Group();
+	/** The orderBys holds a list of how the user wants the listings sorted. */
 	private OrderBy[] orderBys = new OrderBy []{};
+	/** Used to execute queries. */
 	private JdbcTemplate jdbcTemplate;
 	
-	private static String SELECT_COUNT = "select count(*) from Employee ";
-	private static String SELECT_ROW = "select id, firstName, lastName, 5 from Employee ";
+	/** Used for calculating count of employees. */
+	private static final String SELECT_COUNT = "select count(*) from Employee ";
+	/** Used to calculate employee list. */
+	private static final String SELECT_ROW = "select id, firstName, lastName, 5 from Employee ";
 	
 	
+    /** Get the count of employees based on the current generated where clause.
+     * The where clause is generated based on the values. */
+	public int getCount() {
+		return  jdbcTemplate.queryForInt(SELECT_COUNT + constructWhereClause(), extractValues());
+	}
+
+	/**
+	 * Get the employees for the current page of data. 
+	 * @param startItem where to start retrieving records.
+	 * @param how many records to retrieve.
+	 */
+	@SuppressWarnings("unchecked")
+	public List list(int startItem, int numItems) {
+		String query = SELECT_ROW + constructWhereClause() + constructOrderByClause() + 
+		" LIMIT " + (startItem + numItems) + " OFFSET " + startItem;
+		return jdbcTemplate.query(
+				query, 
+				extractValues(), new EmployeeReportObjectMapper());
+	}
+
+	/**
+	 * Get all employees.
+	 */
+	@SuppressWarnings("unchecked")
+	public List list() {
+		return jdbcTemplate.query(
+				SELECT_ROW + constructWhereClause() + constructOrderByClause(), 
+				extractValues(), new EmployeeReportObjectMapper());
+	}
+
+
+	/** Group gets manipualted by the paginator. */
+	public Group group() {
+		return group;
+	}
+
+	/** Order by gets used by the paginator class. */
+	public OrderBy[] orderBy() {
+		return this.orderBys;
+	}
+
+	/** Not used. */
+	public void setFetches(Fetch[] fetches) {
+	}
+
+	/** Not used. */
+	public Fetch[] fetches() {
+		return null;
+	}
+
+	
+	public void setOrderBy(OrderBy[] orderBy) {
+		this.orderBys = orderBy;
+	}
+
+	/** Used for dependency injection of jdbcTemplate. */
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	/** Construct the where clause from the group of Comparison objects. 
+	 * @return the where clause based on the group of comparisons
+	 */
+	protected String constructWhereClause() {
+		StringBuilder builder = new StringBuilder();
+		/* If the group has something in it build a where clause. */
+		if (group.size() > 0) {
+			builder.append(" WHERE ");
+			/* If the current iteration is a comparison, use it. */
+			for (Criterion criterion : group) {
+				if (criterion instanceof Comparison) {
+					Comparison comparison = (Comparison) criterion;
+					if (comparison.getName().equals("taskCount")) {
+						continue;
+					}
+					builder.append(comparison.getName());
+					builder.append(" " + comparison.getOperator().getOperator());
+					builder.append(" ? ");
+					builder.append("AND ");
+				}
+			}
+			return builder.toString().substring(0,builder.length()-4);
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Construct the order by clause.  
+	 * @return
+	 */
+	protected String constructOrderByClause() {
+		StringBuilder builder = new StringBuilder();
+		if (this.orderBys.length > 0) {
+			builder.append(" ORDER BY ");
+			for (OrderBy orderBy : orderBys) {
+				builder.append(" " + orderBy.getName() + " " + orderBy.getDirection() + ",");
+			}
+			
+			return builder.toString().substring(0,builder.length()-1);
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Extract the values for the prepared statement calls.
+	 * @return extracted values.
+	 */
 	protected Object[] extractValues() {
 		List <Object> values = new ArrayList<Object>();
 		for (Criterion criterion : group) {
@@ -36,90 +150,6 @@ public class EmployeeDataSource implements FilteringPagingDataSource {
 			}
 		}
 		return values.toArray(new Object[values.size()]);
-	}
-
-	public int getCount() {
-		
-		int count = jdbcTemplate.queryForInt(SELECT_COUNT + constructWhereClause(), extractValues());
-		System.out.println("Count = " + count);
-		return count;
-	}
-
-	public List list(int startItem, int numItems) {
-		String query;
-		query = SELECT_ROW + constructWhereClause() + constructOrderByClause() + 
-		" LIMIT " + (startItem + numItems) + " OFFSET " + startItem;
-		System.out.println("EmployeeDataSource.list(start, num): query = " + query);
-		return jdbcTemplate.query(
-				query, 
-				extractValues(), new EmployeeReportObjectMapper());
-	}
-
-	public List list() {
-		System.out.println("EmployeeDataSource.list(): query = " + SELECT_ROW + constructWhereClause() + " " + extractValues());
-		return jdbcTemplate.query(
-				SELECT_ROW + constructWhereClause() + constructOrderByClause(), 
-				extractValues(), new EmployeeReportObjectMapper());
-	}
-
-
-	public Group group() {
-		return group;
-	}
-
-	public OrderBy[] orderBy() {
-		return this.orderBys;
-	}
-
-	public void setFetches(Fetch[] fetches) {
-	}
-
-	public Fetch[] fetches() {
-		return null;
-	}
-
-	public void setOrderBy(OrderBy[] orderBy) {
-		this.orderBys = orderBy;
-	}
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
-	protected String constructWhereClause() {
-		StringBuilder builder = new StringBuilder();
-		if (group.size() > 0) {
-			builder.append(" WHERE ");
-			for (Criterion criterion : group) {
-				if (criterion instanceof Comparison) {
-					Comparison comparison = (Comparison) criterion;
-					if (comparison.getName().equals("taskCount")) {
-						continue;
-					}
-					builder.append(comparison.getName());
-					builder.append(" " + comparison.getOperator().getOperator());
-					builder.append(" ? ");
-				}
-			}
-			
-			return builder.toString();
-		} else {
-			return "";
-		}
-	}
-
-	protected String constructOrderByClause() {
-		StringBuilder builder = new StringBuilder();
-		if (this.orderBys.length > 0) {
-			builder.append(" ORDER BY ");
-			for (OrderBy orderBy : orderBys) {
-				builder.append(" " + orderBy.getName() + " " + orderBy.getDirection() + ",");
-			}
-			
-			return builder.toString().substring(0,builder.length()-1);
-		} else {
-			return "";
-		}
 	}
 	
 }

c28937ecdf73f6aed0d168e8343d065f0eba02bb
RichardHightower <RichardHightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-11-13 23:06:56 +0000

    Reviewed with Paul T

--- crank-core/src/main/java/org/crank/message/SimpleMessageManager.java
+++ crank-core/src/main/java/org/crank/message/SimpleMessageManager.java
@@ -5,14 +5,43 @@ import java.util.List;
 
 public class SimpleMessageManager implements MessageManager {
 
-	private List<String> messages = new ArrayList<String>();
+	private List<String> statusMessages = new ArrayList<String>();
+	private List<String> errorMessages = new ArrayList<String>();
+	private List<String> fatalMessages = new ArrayList<String>();
+	private List<String> warningMessages = new ArrayList<String>();
 	
 	public void addStatusMessage(String message) {
-		messages.add(message);
+		statusMessages.add(message);
 	}
 
-	public List<String> getMessages() {
-		return messages;
+
+	public void addErrorMessage(String message) {
+		this.errorMessages.add(message);
+	}
+
+	public void addFatalMessage(String message) {
+		this.fatalMessages.add(message);
+		
+	}
+
+	public void addWarningMessage(String message) {
+		this.warningMessages.add(message);
+	}
+
+	public List<String> getErrorMessages() {
+		return this.errorMessages;
+	}
+
+	public List<String> getFatalMessages() {
+		return this.fatalMessages;
+	}
+
+	public List<String> getStatusMessages() {
+		return statusMessages;
+	}
+
+	public List<String> getWarningMessages() {
+		return this.warningMessages;
 	}
 
 }

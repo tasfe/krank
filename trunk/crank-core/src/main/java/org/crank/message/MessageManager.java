f4f3c7be8b1d87fe4a3ac94407b8d4945f5e3afa
RichardHightower <RichardHightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-11-13 23:06:56 +0000

    Reviewed with Paul T

--- trunk/crank-core/src/main/java/org/crank/message/MessageManager.java
+++ trunk/crank-core/src/main/java/org/crank/message/MessageManager.java
@@ -5,6 +5,12 @@ import java.util.List;
 public interface MessageManager {
 	
 	public void addStatusMessage(String message);
-
-	public List<String> getMessages();
+	public void addErrorMessage(String message);
+	public void addFatalMessage(String message);
+	public void addWarningMessage(String message);
+	
+	public List<String> getStatusMessages();
+	public List<String> getErrorMessages();
+	public List<String> getFatalMessages();
+	public List<String> getWarningMessages();
 }
--- trunk/crank-core/src/main/java/org/crank/message/NoOpMessageManager.java
+++ trunk/crank-core/src/main/java/org/crank/message/NoOpMessageManager.java
@@ -7,11 +7,34 @@ public class NoOpMessageManager implements MessageManager {
 
 	public void addStatusMessage(String message) {
 	}
+	
+	public void addErrorMessage(String message) {
+	}
+
+	public void addFatalMessage(String message) {
+	}
+
+	public void addWarningMessage(String message) {
+	}
 
 	@SuppressWarnings("unchecked")
-	public List<String> getMessages() {
+	public List<String> getStatusMessages() {
 		return Collections.EMPTY_LIST;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<String> getErrorMessages() {
+		return Collections.EMPTY_LIST;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<String> getFatalMessages() {
+		return Collections.EMPTY_LIST;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<String> getWarningMessages() {
+		return Collections.EMPTY_LIST;
+	}
 
 }

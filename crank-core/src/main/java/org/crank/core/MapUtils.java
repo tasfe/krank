68afffa81df1c5d1bf20eeb1967819c62f56ba2d
RichardHightower <RichardHightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2007-12-13 20:06:05 +0000

    prbly no comps

--- crank-core/src/main/java/org/crank/core/MapUtils.java
+++ crank-core/src/main/java/org/crank/core/MapUtils.java
@@ -1,7 +1,7 @@
 package org.crank.core;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.crank.core.spring.support.SpringBeanWrapperPropertiesUtil;
@@ -10,8 +10,8 @@ public class MapUtils {
     private static PropertiesUtil propertiesUtil = new SpringBeanWrapperPropertiesUtil();
     
     @SuppressWarnings("unchecked")
-    public static Map convertListToMap(String propertyName, List list) {
-        Map map = new HashMap(list.size());
+    public static Map convertListToMap(String propertyName, Collection list) {
+        Map map = new LinkedHashMap(list.size());
         for (Object object : list) {
             map.put( propertiesUtil.getPropertyValue( propertyName, object ), object );
         }
@@ -19,7 +19,7 @@ public class MapUtils {
     }
     @SuppressWarnings("unchecked")
     public static Map convertArrayToMap(String propertyName, Object [] array) {
-        Map map = new HashMap(array.length);
+        Map map = new LinkedHashMap(array.length);
         for (Object object : array) {
             map.put( propertiesUtil.getPropertyValue( propertyName, object ), object );
         }

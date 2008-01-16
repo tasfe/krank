d10901595d1fa583d70a1f5c7cfaf9d5f2dde600
tcellucci <tcellucci@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-01-16 18:58:45 +0000

    added unit test for TypeUtils

--- crank-core/src/test/java/org/crank/core/TypeUtilsTest.java
+++ crank-core/src/test/java/org/crank/core/TypeUtilsTest.java
@@ -98,18 +98,149 @@ public class TypeUtilsTest {
         assertTrue(TypeUtils.isText(Employee.class, "address.line1"));
     }
 
-    public void testIsBoolean() {
-        assertTrue(TypeUtils.isBoolean(Test.class, "bool"));
-        assertFalse(TypeUtils.isBoolean(Test.class, "text"));
-    }
-
     @org.testng.annotations.Test
     public void testIsDate() {
         assertFalse(TypeUtils.isDate(Test.class, "bool"));
         assertFalse(TypeUtils.isDate(Test.class, "text"));
         assertTrue(TypeUtils.isDate(Test.class, "date"));
         assertTrue(TypeUtils.isDate(Test.class, "sqlDate"));
-
     }
-
+    
+    @org.testng.annotations.Test
+    public void testIsUrl() {
+    	
+    	assertFalse(TypeUtils.isUrl("nourlddotcom"));
+    	assertTrue(TypeUtils.isUrl("nourlddot.com"));
+    }
+    
+    @org.testng.annotations.Test
+    public void testIsInCollection() {    	
+    	assertFalse(TypeUtils.isInCollection("one", "two,three,four,five"));
+    	assertTrue(TypeUtils.isInCollection("two", "two,three,four,five"));
+    	assertTrue(TypeUtils.isInCollection("five", "two,three,four,five"));
+    	assertTrue(TypeUtils.isInCollection("three", "two,three,four,five"));
+    	assertFalse(TypeUtils.isInCollection("three", "twothreefourfive"));
+    	assertFalse(TypeUtils.isInCollection("three", "two,threefourfive"));
+    	assertFalse(TypeUtils.isInCollection("three", "twothree,fourfive"));
+    }  
+    
+    @org.testng.annotations.Test
+    public void testIsBoolean() {    
+    	assertTrue(TypeUtils.isBoolean(BooleanTestSupport.class, "primitiveBoolean"));
+    	assertTrue(TypeUtils.isBoolean(BooleanTestSupport.class, "wrapperBoolean"));
+    	assertFalse(TypeUtils.isBoolean(BooleanTestSupport.class, "notABoolean"));
+    	assertTrue(TypeUtils.isBoolean(BooleanTestSupport.class, "testSupport.testSupport.primitiveBoolean"));
+    	
+    	try {
+    		TypeUtils.isBoolean(null, "test");
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+       	try {
+    		TypeUtils.isBoolean(BooleanTestSupport.class, null);
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+    	
+    	try {
+    		TypeUtils.isBoolean(BooleanTestSupport.class, "notAProperty");
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+    	
+    }
+    
+    @org.testng.annotations.Test
+    public void testIsEnum() {    
+    	assertTrue(TypeUtils.isEnum(EnumTestSupport.class, "testEnum"));
+    	assertFalse(TypeUtils.isEnum(EnumTestSupport.class, "notAnEnum"));
+    	assertTrue(TypeUtils.isEnum(EnumTestSupport.class, "testSupport.testSupport.testEnum"));
+    	
+    	try {
+    		TypeUtils.isEnum(null, "test");
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+       	try {
+    		TypeUtils.isEnum(EnumTestSupport.class, null);
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+    	
+    	try {
+    		TypeUtils.isEnum(BooleanTestSupport.class, "notAProperty");
+    		assertFalse(true);
+    	}
+    	catch(CrankException ce) {
+    		// expected
+    	}
+    	
+    }    
+    
+   	static class EnumTestSupport {
+		enum TestEnum { one, two, three};
+		private TestEnum testEnum = TestEnum.three;
+		private Object notAnEnum;
+		private EnumTestSupport testSupport;
+		public TestEnum getTestEnum() {
+			return testEnum;
+		}
+		public void setTestEnum(TestEnum testEnum) {
+			this.testEnum = testEnum;
+		}
+		public Object getNotAnEnum() {
+			return notAnEnum;
+		}
+		public void setNotAnEnum(Object notAnEnum) {
+			this.notAnEnum = notAnEnum;
+		}
+		public EnumTestSupport getTestSupport() {
+			return testSupport;
+		}
+		public void setTestSupport(EnumTestSupport testSupport) {
+			this.testSupport = testSupport;
+		}
+   	}
+    
+   	class BooleanTestSupport {
+		private boolean primitiveBoolean;
+		private Boolean wrapperBoolean;
+		private Object notABoolean;
+		private BooleanTestSupport testSupport;
+		public boolean isPrimitiveBoolean() {
+			return primitiveBoolean;
+		}
+		public void setPrimitiveBoolean(boolean primitiveBoolean) {
+			this.primitiveBoolean = primitiveBoolean;
+		}
+		public Boolean getWrapperBoolean() {
+			return wrapperBoolean;
+		}
+		public void setWrapperBoolean(Boolean wrapperBoolean) {
+			this.wrapperBoolean = wrapperBoolean;
+		}
+		public Object getNotABoolean() {
+			return notABoolean;
+		}
+		public void setNotABoolean(Object notABoolean) {
+			this.notABoolean = notABoolean;
+		}
+		public BooleanTestSupport getTestSupport() {
+			return testSupport;
+		}
+		public void setTestSupport(BooleanTestSupport testSupport) {
+			this.testSupport = testSupport;
+		}
+	};
+    
 }

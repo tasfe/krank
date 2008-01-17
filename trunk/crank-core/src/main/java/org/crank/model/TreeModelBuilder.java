b68417e670b717c5f27ab22763c35b19d8790cf1
tcellucci <tcellucci@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-01-17 00:06:38 +0000

    added tests for TreeModelBuilder added generic signature for TreeModelBuilder and AbstractTreeModelBuilder

--- trunk/crank-core/src/main/java/org/crank/model/TreeModelBuilder.java
+++ trunk/crank-core/src/main/java/org/crank/model/TreeModelBuilder.java
@@ -3,13 +3,13 @@ package org.crank.model;
 import java.util.List;
 
 /** Builds a tree model for a tree component from a list. */
-public interface TreeModelBuilder {
+public interface TreeModelBuilder<T> {
 
 	/**
 	 * Build a tree based on a java.util.List
 	 * @param list
 	 * @return the new tree model object.
 	 */
-	public Object createTreeModelFromList(List list);
+	public T createTreeModelFromList(List<?> list);
 
 }

8c7f85bd15a8be6080e821be9930cb23db0b0aeb
richardhightower <richardhightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-04-22 18:32:25 +0000

    Added eq and hash

--- examples/employee-jpa-model/src/main/java/org/crank/crud/model/Category.java
+++ examples/employee-jpa-model/src/main/java/org/crank/crud/model/Category.java
@@ -32,4 +32,23 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+
+        Category category = (Category) o;
+
+        if (id != null ? !id.equals(category.id) : category.id != null) return false;
+        if (name != null ? !name.equals(category.name) : category.name != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = (id != null ? id.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }

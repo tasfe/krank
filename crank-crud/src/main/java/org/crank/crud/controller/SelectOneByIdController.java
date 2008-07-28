b2e7becd0100448c0e0c54b434b4c777c3bda8eb
richardhightower <richardhightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-07-28 21:05:50 +0000

    changes for JD's select one fun

--- crank-crud/src/main/java/org/crank/crud/controller/SelectOneByIdController.java
+++ crank-crud/src/main/java/org/crank/crud/controller/SelectOneByIdController.java
@@ -1,33 +1,72 @@
 package org.crank.crud.controller;
 
 import java.io.Serializable;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.crank.crud.criteria.Comparison;
 
+/**
+ *  This class allows you to select an id into an object where a true JPA relationship does not exist.
+ *
+ */
 public abstract class SelectOneByIdController
 	<PT extends Serializable, T extends Serializable, PK extends Serializable> 
 	extends SelectByIdController<PT, T, PK>{
 
-	private T clickedItem;
-	
-	@SuppressWarnings("unchecked")
+    /** The item that the user clicked on in the listing. */
+    private T clickedItem;
+
+    private boolean toParent;
+
+    public boolean isToParent() {
+        return toParent;
+    }
+
+    public void setToParent(boolean toParent) {
+        this.toParent = toParent;
+    }
+
+    /**
+     * This method gets called when the user clicks on an item in the listing. 
+     */
+    @SuppressWarnings("unchecked")
 	public void process() {
-		BeanWrapper child = new BeanWrapperImpl(clickedItem);
-		child.setPropertyValue(super.targetProperty, getParentId());
-		Set<T> unselectedChildren = getSelectedChildren();
-		for (T unSelect : unselectedChildren) {
-			BeanWrapper unChild = new BeanWrapperImpl(unSelect);
-			unChild.setPropertyValue(targetProperty, null);
-		}
-		repo.merge(unselectedChildren);
-		repo.merge(clickedItem);		
-		this.setShow(false);
+        /* retrieve the item selected and wrap it in a bean wrapper. */
+        BeanWrapper child = new BeanWrapperImpl(clickedItem);
+
+        if (!toParent) {
+            /* Set the selected id into child. */
+            child.setPropertyValue(super.targetProperty, getParentId());
+        } else {
+            getParent().setPropertyValue(super.targetProperty, child.getPropertyValue(super.sourceProperty));
+        }
+
+        /* Unselect whatever was selected. */
+        if (!toParent) {
+            Set<T> unselectedChildren = getSelectedChildren();
+            for (T unSelect : unselectedChildren) {
+                BeanWrapper unChild = new BeanWrapperImpl(unSelect);
+                unChild.setPropertyValue(super.targetProperty, null);
+            }
+            /* Merge the changes with the database. */
+            updateChildren(unselectedChildren);
+        } else {
+            //
+        }
+
+
+        /* Quit showing the listing to the end user. */
+        this.setShow(false);
 	}
 
-	public T getClickedItem() {
+    protected void updateChildren(Set<T> unselectedChildren) {
+        repo.merge(unselectedChildren);
+        repo.merge(clickedItem);                    
+    }
+
+    public T getClickedItem() {
 		return clickedItem;
 	}
 
@@ -45,5 +84,33 @@ public abstract class SelectOneByIdController
 		}
 		
 	}
+
+    protected Set<T> getSelectedChildren() {
+
+        /* If we are setting the id in the child, let's do that here. */
+        if (!toParent) {
+            BeanWrapper parent = getParent();
+            PK parentId = getParentId(parent);
+            if (parentId == null) {
+                return Collections.emptySet();
+            }
+
+            return new TreeSet<T>(findSelectedChildren(targetProperty, parentId));
+        } else {
+            /* If we are setting the id into the parent, let's find out which child is selected here. */
+            /* Look up the current value in the parent. */
+            Object value = getParent().getPropertyValue(targetProperty);
+            if (value == null) {
+                return Collections.emptySet();
+            }
+            /* Look up the child by the sourceProperty with the value that we just pulled from the parent. */
+            return new TreeSet<T>(findSelectedChildren(sourceProperty, value));
+        }
+    }
+
+    protected List<T> findSelectedChildren(String property, Object value) {
+            return repo.find(Comparison.eq(property, value));
+    }
+
 
 }

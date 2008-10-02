d047dcb0fcecbaeb61150452357f4a27d40617b4
richardhightower <richardhightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-10-02 21:35:19 +0000

    Simplified usage by codifying defaults.

--- trunk/crank-crud/src/main/java/org/crank/crud/GenericDaoFactory.java
+++ trunk/crank-crud/src/main/java/org/crank/crud/GenericDaoFactory.java
@@ -5,7 +5,12 @@ import java.util.List;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 import org.crank.crud.cache.PreloadConfiguration;
 import org.crank.crud.cache.PreloadableCacheableGenericDaoJpa;
@@ -17,7 +22,7 @@ import org.crank.crud.cache.CachingAdvisor;
  *  @version $Revision:$
  *  @author Rick Hightower
  */
-public class GenericDaoFactory extends ProxyFactoryBean implements InitializingBean {
+public class GenericDaoFactory extends ProxyFactoryBean implements InitializingBean, ApplicationContextAware {
 
     private Class<?> intf;
 
@@ -26,8 +31,33 @@ public class GenericDaoFactory extends ProxyFactoryBean implements InitializingB
     private EntityManagerFactory entityManagerFactory;
     
     private List<QueryHint<?>> queryHints;
+    
+    private TransactionInterceptor transactionInterceptor;
+    
+    private PlatformTransactionManager transactionManager;
+    
+    private String transactionManagerName = "transactionManager";
+    
+    private String entityManagerFactoryName = "entityManagerFactory";
 
-    private boolean preloadEnabled = false;
+    public void setEntityManagerFactoryName(String entityManagerFactoryName) {
+		this.entityManagerFactoryName = entityManagerFactoryName;
+	}
+
+	public void setTransactionManagerName(String transactionManagerName) {
+		this.transactionManagerName = transactionManagerName;
+	}
+
+	public void setTransactionInterceptor(
+			TransactionInterceptor transactionInterceptor) {
+		this.transactionInterceptor = transactionInterceptor;
+	}
+
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
+	}
+
+	private boolean preloadEnabled = false;
     {
     	if(preloadEnabled) {
     		
@@ -52,9 +82,13 @@ public class GenericDaoFactory extends ProxyFactoryBean implements InitializingB
         this.intf = intf;
     }
 
+    @Deprecated
     public GenericDaoFactory( TransactionInterceptor transactionInterceptor ) {
-        this.addAdvice( transactionInterceptor );
+        this.transactionInterceptor = transactionInterceptor;
     }
+    
+    public GenericDaoFactory() {
+    }    
 
     @SuppressWarnings( "unchecked" )
     public void afterPropertiesSet() throws Exception {
@@ -62,7 +96,7 @@ public class GenericDaoFactory extends ProxyFactoryBean implements InitializingB
             throw new RuntimeException( "The business object property must be set." );
         }
         if (intf == null) {
-            throw new RuntimeException( "The interface property must be set." );
+        	intf = GenericDao.class;
         }
 
         this.setInterfaces( new Class[] { intf } );
@@ -72,18 +106,36 @@ public class GenericDaoFactory extends ProxyFactoryBean implements InitializingB
         if (isCachingEnabled()) {
             dao = loadCachingDao();
         } else {
-            dao = loadNonCachingDao();
+            dao = createNonCachingDao();
         }
 
         this.setTarget( dao );
+        if (transactionInterceptor == null) {
+            if (transactionManager == null) {
+            	if (applicationContext.containsBean(transactionManagerName)) {
+            		transactionManager = (PlatformTransactionManager) applicationContext.getBean(this.transactionManagerName);
+            	} else {
+            		throw new RuntimeException (String.format("transactionManager property not set and transactionPropertyName %s not found", this.transactionManagerName));
+            	}
+            }
+        	transactionInterceptor = new TransactionInterceptor(transactionManager, new AnnotationTransactionAttributeSource());
+        } 
+        this.addAdvice(this.transactionInterceptor);
         this.addAdvisor( new DaoMethodsIntroductionAdvisor() );
 
     }
     
     @SuppressWarnings("unchecked")
-    private GenericDaoJpa loadNonCachingDao() {
+    private GenericDaoJpa createNonCachingDao() {
         GenericDaoJpa dao = new GenericDaoJpa( bo );
         dao.setNewSelectStatement(newSelect);
+        if (entityManagerFactory==null) {
+        	if (this.applicationContext.containsBean(entityManagerFactoryName)) {
+        		this.entityManagerFactory = (EntityManagerFactory) this.applicationContext.getBean(this.entityManagerFactoryName);
+        	} else {
+        		throw new RuntimeException(String.format("entityManagerFactory was not set and entityManagerFactoryName not found (%s)", this.entityManagerFactoryName));
+        	}
+        }
         dao.setEntityManagerFactory( entityManagerFactory );
         dao.setQueryHints(queryHints);
         return dao;
@@ -120,5 +172,12 @@ public class GenericDaoFactory extends ProxyFactoryBean implements InitializingB
 
 	public void setQueryHints(List<QueryHint<?>> queryHints) {
 		this.queryHints = queryHints;
+	}
+
+	private ApplicationContext applicationContext;
+	
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
 	}
 }

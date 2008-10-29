54166c05205268b92263cadc652a3c8c1a2e9564
richardhightower <richardhightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2008-10-29 19:54:15 +0000

    pom refactor step 2

--- examples/crank-crud-webapp/runjetty.sh
+++ examples/crank-crud-webapp/runjetty.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAVEN_OPTS="-Xdebug -Xnoagent -Dsun.lang.ClassLoader.allowArraySyntax=true -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9009 -ea"
+MAVEN_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9009 -ea"
 export MAVEN_OPTS
 echo $MAVEN_OPTS
 mvn -Ddb=mysql -Dlog4j.configuration=file:./log4j.xml  jetty:run

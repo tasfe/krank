b59c8544bd1ac533774077db3b305b7521a0dfb2
RichardHightower <RichardHightower@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2009-02-04 19:42:08 +0000

    Took out compile time dependency on Oracle.

--- crank-codegen/src/main/groovy/com/arcmind/codegen/oracle/OracleDataBaseMetaDataReader.groovy
+++ crank-codegen/src/main/groovy/com/arcmind/codegen/oracle/OracleDataBaseMetaDataReader.groovy
@@ -5,7 +5,9 @@ package com.arcmind.codegen.oracle
 
 import com.arcmind.codegen.DataBaseMetaDataReader
 import com.arcmind.codegen.Table
-import oracle.jdbc.OracleDatabaseMetaData;
+
+//Let's not import this so we don't depend on a dependency that does not exist in the public repos
+//import oracle.jdbc.OracleDatabaseMetaData;
 import java.sql.ResultSet
 
 
@@ -38,47 +40,47 @@ class OracleDataBaseMetaDataReader extends DataBaseMetaDataReader {
      */
     def processTables() {
     	  // Get the database meta data
-    	  OracleDatabaseMetaData dmd = (OracleDatabaseMetaData)this.connection.getMetaData();
+    	  //OracleDatabaseMetaData dmd = (OracleDatabaseMetaData)this.connection.getMetaData();
     	  
     	  // Get schemas
-    	  ResultSet rs = dmd.getSchemas()
+    	  //ResultSet rs = dmd.getSchemas()
           ResultSet rs1 = null;
     	  
-          while (rs.next()) {
-        	  if (schema.equals(rs.getString(1))) {
-		    	  //Get tables
-		          rs1 = dmd.getTables(null,rs.getString(1),"%",tableTypes)
-		    	  
-			          jdbcUtils.iterate(rs1, { ResultSet resultSet ->
-			                String tableName = resultSet.getString (3)
-			                if (debug) println "DataBaseMetaDataReader: processTables() tableName=${tableName}"
-			                tables << new Table(name:tableName)
-			            })
-        	  }
-          }
+//          while (rs.next()) {
+//        	  if (schema.equals(rs.getString(1))) {
+//		    	  //Get tables
+//		          rs1 = dmd.getTables(null,rs.getString(1),"%",tableTypes)
+//
+//			          jdbcUtils.iterate(rs1, { ResultSet resultSet ->
+//			                String tableName = resultSet.getString (3)
+//			                if (debug) println "DataBaseMetaDataReader: processTables() tableName=${tableName}"
+//			                tables << new Table(name:tableName)
+//			            })
+//        	  }
+//          }
     }
     
     // For debugging
     public void listWholeOracleDb() {
   	  // Get the database meta data
-  	  OracleDatabaseMetaData dmd = (OracleDatabaseMetaData)this.connection.getMetaData();
+//  	  OracleDatabaseMetaData dmd = (OracleDatabaseMetaData)this.connection.getMetaData();
   	  
   	  // Get schemas
-  	  ResultSet rs = dmd.getSchemas();
-        ResultSet rs1 = null;
-  	  
-        println("The following Schemas are available in the database:");
-        rs = dmd.getSchemas();
-        rs1 = null;
-        while(rs.next()) {
-          println(rs.getString(1));
-          println(
-              "The following tables are available in the "+rs.getString(1)+" schema:");
-          rs1 = dmd.getTables(null,rs.getString(1),"%",null);
-          while(rs1.next()) {
-            System.out.println("   "+ rs1.getString(3)+" : "+rs1.getString(4));
-          }
-        }         	
+//  	  ResultSet rs = dmd.getSchemas();
+//        ResultSet rs1 = null;
+//
+//        println("The following Schemas are available in the database:");
+//        rs = dmd.getSchemas();
+//        rs1 = null;
+//        while(rs.next()) {
+//          println(rs.getString(1));
+//          println(
+//              "The following tables are available in the "+rs.getString(1)+" schema:");
+//          rs1 = dmd.getTables(null,rs.getString(1),"%",null);
+//          while(rs1.next()) {
+//            System.out.println("   "+ rs1.getString(3)+" : "+rs1.getString(4));
+//          }
+//        }
     }
 
 }

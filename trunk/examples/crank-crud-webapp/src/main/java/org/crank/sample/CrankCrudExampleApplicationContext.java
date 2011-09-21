1c3b571526b5c51bbc77c3a08821f0d6f612b842
wbogaardt@gmail.com <wbogaardt@gmail.com@2cce82a7-ba2d-0410-917d-c9ec71ddbe0a>
2011-09-21 18:10:43 +0000

    fixed managed object for Employee, address.line1 was address.line_1 in the model object, so the employee listing was not showing address line 1 data, in the example app.

--- trunk/examples/crank-crud-webapp/src/main/java/org/crank/sample/CrankCrudExampleApplicationContext.java
+++ trunk/examples/crank-crud-webapp/src/main/java/org/crank/sample/CrankCrudExampleApplicationContext.java
@@ -77,7 +77,7 @@ public abstract class CrankCrudExampleApplicationContext extends CrudJSFConfig {
 		if (managedObjects == null) {
 			managedObjects = new ArrayList<CrudManagedObject>();
 			CrudManagedObject crudManagedObject =  CrudManagedObject.createWithPropertiesAndDAO(Employee.class,
-					EmployeeDAO.class, "firstName","lastName","status","active","dob","age","phone","department.name","address.zipCode", "address.line1", "address.line2");
+					EmployeeDAO.class, "firstName","lastName","status","active","dob","age","phone","department.name","address.zipCode", "address.line_1", "address.line2");
 			//crudManagedObject.setTransactionalController(true);
 			managedObjects.add(crudManagedObject);
 			managedObjects.add(new CrudManagedObject(Department.class,
